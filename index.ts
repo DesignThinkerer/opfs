eeb3fe2a544e9043e90d3d5b5cf7a6ae9c7d276e
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2023-12-31 02:35:45 +0000

    add recommended compilerOptions for a Bun project (https://bun.sh/docs/typescript#suggested-compileroptions)

--- index.ts
+++ index.ts
@@ -2,10 +2,12 @@ import figlet from "figlet";
 
 const server = Bun.serve({
     fetch() {
-        const body = figlet.textSync( "Bun!");
+        const body = figlet.textSync(`Bun v${Bun.version}`);
         return new Response(body);
     },
     port: 3000,
 });
+
+console.log(Bun.version);
 
 console.log(`Listening on http://localhost:${server.port} ...`);
