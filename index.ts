2b76875e710ab381562cf6fb711a3e7976b55dc1
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2023-12-31 02:46:09 +0000

    added serving index.html

--- index.ts
+++ index.ts
@@ -1,11 +1,23 @@
 import figlet from "figlet";
 
 const server = Bun.serve({
-    fetch() {
-        const body = figlet.textSync(`Bun v${Bun.version}`);
-        return new Response(body);
-    },
     port: 3000,
+    async fetch(req) {
+        const url = new URL(req.url);
+
+        // return index.html for root path
+        if (url.pathname === "/"){
+            return new Response(Bun.file("index.html"), {
+                headers: {
+                    "Content-Type": "text/html",
+                },
+            });
+        }
+
+
+        const body = figlet.textSync(`Not found --- Bun v${Bun.version}`);
+        return new Response(body, { status: 404 });
+    },
 });
 
 console.log(Bun.version);
